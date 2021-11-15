3662297228da2771b6d3b24ca10d9bfa285670d4
Azkadev <azkadev@pop-os.localdomain>
2021-11-16 06:47:05 +0700

    Support More Language Code

---
+++ Google-Apps-Script/api.js
@@ -0,0 +1,66 @@
+var lib = class Api {
+  
+    constructor(token, options) {
+      this.token = token;
+      this.options = options;
+    }
+  
+    request(method, data, form, blob) {
+      if (!this.token) {
+        return 'Bot Token is required';
+      }
+      if (!method) {
+        return 'Method is required';
+      }
+      var options = {
+        'method': 'post',
+        'contentType': 'application/json'
+      };
+      if (data) {
+        options['payload'] = JSON.stringify(data);
+      }
+      if (form) {
+        options = { 'method': 'post' }
+        if (data) options['payload'] = data;
+      }
+      var url = `${this.options.api}${this.options.type}${this.token}/${method}`;
+      var response = UrlFetchApp.fetch(url, options);
+      if (response.getResponseCode() == 200) {
+        if (blob) {
+          return response.getBlob();
+        } else {
+          return JSON.parse(response.getContentText());
+        }
+      }
+      return false;
+    }
+  
+    invoke(json, object) {
+      if (object) {
+        for (var key in object) {
+          if (Object.hasOwnProperty.call(object, key)) {
+            var loop_data = object[key];
+            json[key] = loop_data;
+          }
+        }
+        return json;
+      } else {
+        return json;
+      }
+    }
+  
+    invokeSync(json, object) {
+      if (object) {
+        for (var key in object) {
+          if (Object.hasOwnProperty.call(object, key)) {
+            var loop_data = object[key];
+            json[key] = loop_data;
+          }
+        }
+        return json;
+      } else {
+        return json;
+      }
+    }
+  
+  }
---
+++ Google-Apps-Script/telegram.js
@@ -0,0 +1,86 @@
+/// azka dev
+
+var telegram = class Telegram {
+
+    constructor(token, options = {}) {
+        this.optiondef = {
+            type: "bot",
+            api: false
+        };
+        if (options.api) {
+            if (RegExp("(\/$|^https?)", "i").exec(options.api)) {
+                this.optiondef.api = options.api;
+            } else {
+                throw {
+                    "message": "tolong isi value dengan benar"
+                };
+            }
+        } else {
+            options.api = 'https://api.telegram.org/';
+        }
+        this.options = this.invokeSync(this.optiondef, options);
+        this.token = token;
+        this.api = new lib(token, this.options);
+    }
+
+
+
+    loginUser(phone, url) {
+        var options = {
+            'method': 'post',
+            'contentType': 'application/json'
+        };
+        options['payload'] = JSON.stringify(
+            {
+                "phone_number": phone
+            }
+        );
+
+        var url = `${url}/userlogin`;
+        var response = UrlFetchApp.fetch(url, options);
+        if (response.getResponseCode() == 200) {
+            return JSON.parse(response.getContentText());
+
+        }
+        return false;
+    }
+
+    invokeSync(json, object) {
+        if (object) {
+            for (var key in object) {
+                if (Object.hasOwnProperty.call(object, key)) {
+                    var loop_data = object[key];
+                    json[key] = loop_data;
+                }
+            }
+            return json;
+        } else {
+            return json;
+        }
+    }
+
+    doPost(e) {
+        if (e.postData.type == "application/json") {
+            var update = JSON.parse(e.postData.contents);
+            if (update) return update;
+        }
+        return false;
+    }
+
+    newBot(token) {
+        var option = this.options;
+        option.type = "bot";
+        return {
+            api: new lib(token, option)
+        };
+    }
+
+    newUser(token) {
+        var option = this.options;
+        option.type = "user";
+        return {
+            api: new lib(token, option)
+        };
+    }
+
+}
