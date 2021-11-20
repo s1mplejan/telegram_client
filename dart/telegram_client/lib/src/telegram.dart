74a68ad2f719a7b9019effc1a2692dcf878a5525
Azkadev <azkadev@pop-os.localdomain>
2021-11-20 07:22:22 +0700

    update

---
+++ dart/telegram_client/lib/src/telegram.dart
@@ -0,0 +1,51 @@
+part of telegram_client;
+
+class Telegram {
+  final String _token;
+  // ignore: prefer_typing_uninitialized_variables
+  final Map? _option;
+  final Map _options = {
+    "botPath": "/bot/",
+    "userPath": "/user/",
+    "port": 8080,
+    "type": "bot",
+    "logger": false,
+    "api": "https://api.telegram.org/"
+  };
+  Telegram(this._token, [this._option]) {
+    if (_token.isEmpty) {
+      throw "please fill token bot";
+    }
+    if (_option != null) {
+      if (_option!.toBoolean && _option!.isType == "object") {
+        _option!.forEach((key, value) {
+          if (key.toString().toLowerCase() == "api") {
+            if (RegExp("\/\$", caseSensitive: false).hasMatch(key.toString())) {
+              _options[key.toString().toLowerCase()] = value;
+            } else {
+              _options[key.toString().toLowerCase()] = "${value.toString()}/";
+            }
+          } else {
+            _options[key.toString().toLowerCase()] = value;
+          }
+        });
+      }
+    }
+  }
+
+  _Request get api {
+    return _Request(_token, _options);
+  }
+
+  _Request newBot(String newToken) {
+    var option = _options;
+    option["type"] = "bot";
+    return _Request(newToken, option);
+  }
+
+  _Request newUser(String newToken) {
+    var option = _options;
+    option["type"] = "user";
+    return _Request(newToken, option);
+  }
+}
---
+++ dart/telegram_client/lib/tdlib/tdlib.dart
@@ -0,0 +1,86 @@
+// ignore_for_file: avoid_types_as_parameter_names
+
+import 'dart:convert' as convert;
+import 'dart:ffi' as ffi;
+import 'package:ffi/ffi.dart' as pkgffi;
+
+class Tdlib {
+  String pathTdl;
+  Tdlib(this.pathTdl);
+
+  // ignore: non_constant_identifier_names
+  ffi.DynamicLibrary TdlibPathFile() {
+    return ffi.DynamicLibrary.open(pathTdl);
+  }
+
+  // ignore: non_constant_identifier_names
+  ffi.Pointer Function() get client_create {
+    return TdlibPathFile()
+        .lookup<ffi.NativeFunction<ffi.Pointer Function()>>(
+            'td_json_client_create')
+        .asFunction();
+  }
+
+  // ignore: non_constant_identifier_names
+  ffi.Pointer<pkgffi.Utf8> Function(ffi.Pointer, double) get client_receive {
+    return TdlibPathFile()
+        .lookup<
+            ffi.NativeFunction<
+                ffi.Pointer<pkgffi.Utf8> Function(
+                    ffi.Pointer, ffi.Double)>>('td_json_client_receive')
+        .asFunction();
+  }
+
+  // ignore: non_constant_identifier_names, unused_local_variable
+  void Function(ffi.Pointer, ffi.Pointer<pkgffi.Utf8>) get client_send {
+    return TdlibPathFile()
+        .lookup<
+            ffi.NativeFunction<
+                ffi.Void Function(ffi.Pointer,
+                    ffi.Pointer<pkgffi.Utf8>)>>('td_json_client_send')
+        .asFunction();
+  }
+
+  // ignore: non_constant_identifier_names, unused_local_variable
+  ffi.Pointer<pkgffi.Utf8> Function(ffi.Pointer, ffi.Pointer<pkgffi.Utf8>)
+      get client_execute {
+    return TdlibPathFile()
+        .lookup<
+            ffi.NativeFunction<
+                ffi.Pointer<pkgffi.Utf8> Function(ffi.Pointer,
+                    ffi.Pointer<pkgffi.Utf8>)>>('td_json_client_execute')
+        .asFunction();
+  }
+
+  // ignore: non_constant_identifier_names, unused_local_variable
+  void Function(ffi.Pointer) get client_destroy {
+    return TdlibPathFile()
+        .lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer)>>(
+            'td_json_client_destroy')
+        .asFunction();
+  }
+
+  clientExecute(client, jsonobject) {
+    var fetch = client_execute.call(
+        client, convert.json.encode(jsonobject).toNativeUtf8());
+    return convert.json.decode(fetch.toDartString());
+  }
+
+  invokeSync(client, jsonobject) {
+    var fetch = client_execute.call(
+        client, convert.json.encode(jsonobject).toNativeUtf8());
+    return convert.json.decode(fetch.toDartString());
+  }
+
+  clientSend(client, jsonsend) {
+    return client_send.call(
+        client, convert.json.encode(jsonsend).toNativeUtf8());
+  }
+
+  clienReceive(client, double) {
+    /*
+    return convert.json.decode(client_receive.call(client, double).toDartString());
+  
+*/
+  }
+}
