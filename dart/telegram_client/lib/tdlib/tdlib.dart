b6d42e8b7083b462fadb294ca5929898d16b787a
Azkadev <azkadev@pop-os.localdomain>
2021-11-20 12:53:54 +0700

    update

--- dart/telegram_client/lib/tdlib/tdlib.dart
+++ dart/telegram_client/lib/tdlib/tdlib.dart
@@ -1,34 +1,46 @@
 part of telegram_client;
 
 class Tdlib {
-  String pathTdl;
-  Map<String, dynamic> optionDefault = {
+  final String _pathTdl;
+  Map<String, dynamic> option;
+  final Map<String, dynamic> _optionDefault = {
     '@type': 'tdlibParameters',
-    'api_id': 273729,
-    'api_hash': '0f7a4f1ed6c06469bf0ecf70ce92b49d',
-    'database_directory': "${io.Platform.script.path.toString()}/telegram",
-    'files_directory': "${io.Platform.script.path.toString()}/telegram",
+    'api_id': 0,
+    'api_hash': '',
+    'database_directory': "",
+    'files_directory': "",
     'enable_storage_optimizer': true,
     'system_language_code': 'en',
+    'new_verbosity_level': 0,
     'application_version': 'v1',
     'device_model': 'dart',
-    'system_version': 'azkadev'
+    'system_version': 'azkadev',
+    "database_key": "" 
   };
-  Tdlib(this.pathTdl, [Map<String, dynamic>? option]) {
-    if (option != null) {
+  late ffi.Pointer client = _client_create();
+
+  Tdlib(this._pathTdl, this.option) {
       option.forEach((key, value) {
-        optionDefault[key.toString().toLowerCase()] = value;
+        _optionDefault[key.toString().toLowerCase()] = value;
       });
-    }
+    
+    var optin = {'@type': 'setTdlibParameters', 'parameters': _optionDefault};
+    clientSend({
+      "@type": "setLogVerbosityLevel",
+      "new_verbosity_level": _optionDefault['new_verbosity_level']
+    });
+    clientSend(optin);
+    clientSend({'@type': 'setDatabaseEncryptionKey', 'new_encryption_key': _optionDefault["database_key"]});
+    clientSend({'@type': 'checkDatabaseEncryptionKey', 'encryption_key': _optionDefault["database_key"]});
   }
 
   // ignore: non_constant_identifier_names
   ffi.DynamicLibrary TdlibPathFile() {
-    return ffi.DynamicLibrary.open(pathTdl);
+    return ffi.DynamicLibrary.open(_pathTdl);
   }
 
   // ignore: non_constant_identifier_names
-  ffi.Pointer Function() get client_create {
+  ffi.Pointer Function() get _client_create {
     return TdlibPathFile()
         .lookup<ffi.NativeFunction<ffi.Pointer Function()>>(
             'td_json_client_create')
@@ -36,7 +48,7 @@ class Tdlib {
   }
 
   // ignore: non_constant_identifier_names
-  ffi.Pointer<pkgffi.Utf8> Function(ffi.Pointer, double) get client_receive {
+  ffi.Pointer<pkgffi.Utf8> Function(ffi.Pointer, double) get _client_receive {
     return TdlibPathFile()
         .lookup<
             ffi.NativeFunction<
@@ -46,7 +58,7 @@ class Tdlib {
   }
 
   // ignore: non_constant_identifier_names, unused_local_variable
-  void Function(ffi.Pointer, ffi.Pointer<pkgffi.Utf8>) get client_send {
+  void Function(ffi.Pointer, ffi.Pointer<pkgffi.Utf8>) get _client_send {
     return TdlibPathFile()
         .lookup<
             ffi.NativeFunction<
@@ -55,9 +67,9 @@ class Tdlib {
         .asFunction();
   }
 
-  // ignore: non_constant_identifier_names, unused_local_variable
   ffi.Pointer<pkgffi.Utf8> Function(ffi.Pointer, ffi.Pointer<pkgffi.Utf8>)
-      get client_execute {
+      // ignore: non_constant_identifier_names
+      get _client_execute {
     return TdlibPathFile()
         .lookup<
             ffi.NativeFunction<
@@ -67,36 +79,48 @@ class Tdlib {
   }
 
   // ignore: non_constant_identifier_names, unused_local_variable
-  void Function(ffi.Pointer) get client_destroy {
+  void Function(ffi.Pointer) get _client_destroy {
     return TdlibPathFile()
         .lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer)>>(
             'td_json_client_destroy')
         .asFunction();
   }
 
-  clientExecute(client, jsonobject) {
-    var fetch = client_execute.call(
-        client, convert.json.encode(jsonobject).toNativeUtf8());
-    return convert.json.decode(fetch.toDartString());
-  }
-
   invokeSync(client, jsonobject) {
     var fetch =
-        client_execute(client, convert.json.encode(jsonobject).toNativeUtf8());
+        _client_execute(client, convert.json.encode(jsonobject).toNativeUtf8());
     return convert.json.decode(fetch.toDartString());
   }
 
-  clientSend(client, jsonsend) {
-    return client_send.call(
-        client, convert.json.encode(jsonsend).toNativeUtf8());
+  invoke(Object? value) {
+    _client_send(client, convert.json.encode(value).toNativeUtf8());
+    return _client_receive(client, 1.0);
+  }
+
+  clientSend(jsonsend) {
+    return _client_send(client, convert.json.encode(jsonsend).toNativeUtf8());
+  }
+
+  void clientDestroy() {
+    return _client_destroy(client);
   }
 
   String clienReceive(client, [double timeout = 10.0]) {
     try {
-      return client_receive(client, timeout).toDartString();
-      // ignore: empty_catches
+      return _client_receive(client, timeout).toDartString();
     } catch (e) {
       return "";
+    }
+  }
+
+  void on(callback, [double timeout = 10.0]) {
+    var nums = 0;
+    while (nums == 0) {
+      var update = clienReceive(client, timeout);
+      if (update.isNotEmpty) {
+        var updateMessage = convert.json.decode(update);
+        callback(updateMessage, client);
+      }
     }
   }
 }
