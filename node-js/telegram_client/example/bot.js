d90d3317b24ab0ca0b40fe84e1cbf4b59df532dc
￴ <>
2022-02-16 21:16:12 +0700

    update

--- node-js/telegram_client/example/bot.js
+++ node-js/telegram_client/example/bot.js
@@ -1,2 +1,11 @@
-var {Telegram } =require("../src/index");
-var lib = new Telegram();
+var { Telegram } = require("../src/index");
+var token_bot = "3131:asjakakmkad";
+var chat_id = 183813;
+var tg =  new Telegram(token_bot).api;
+
+tg.sendMessage(chat_id, "helo world");
+
+tg.requestForm("sendDocument", {
+    "chat_id": chat_id,
+    "document": tg.file("./bot.js")
+});
