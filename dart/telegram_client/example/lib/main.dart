2842ae78a5b8dd1e2644f06da89374472549b025
￴ <>
2022-03-19 20:56:56 +0700

    Update beta test app

--- dart/telegram_client/example/lib/main.dart
+++ dart/telegram_client/example/lib/main.dart
@@ -1,37 +1,34 @@
 import 'package:flutter/material.dart';
+import 'dart:isolate';
+import 'package:telegram_client/telegram_client.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+var path = Directory.current.path;
+var option = {
+  'api_id': 1917085,
+  'api_hash': 'a612212e6ac3ff1f97a99b2e0f050894',
+  'database_directory': "$path/bot",
+  'files_directory': "$path/bot",
+};
+Tdlib tg =
+    Tdlib("/home/azkadev/Downloads/azkauserrobot-1.0.1/libtdjson.so", option);
+    
+void main() async {
+  runApp(
+    MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+      home: MyHomePage(title: 'Flutter Demo Home Page', tg: tg),
+    ),
+  );
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
+  const MyHomePage({Key? key, required this.title, required this.tg})
+      : super(key: key);
+  final Tdlib tg;
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -49,7 +46,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  String text = "hellow rold";
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -59,6 +56,50 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+  }
+
+  @override
+  void initState() async {
+    super.initState();
+    ReceivePort port = ReceivePort();
+
+    await Isolate.spawn(telegram, port.sendPort);
+
+    port.listen((update) async{
+      if (update["@type"] == "updateNewMessage" &&
+          update["message"]["@type"] == "message") {
+        var msg = update["message"];
+        var chatId = msg["chat_id"];
+        if (!msg["is_outgoing"]) {
+          print(update["message"]["content"]);
+          if (typeData(update["message"]["content"]) == "object" &&
+              typeData(update["message"]["content"]["text"]) == "object") {
+            setState(() {
+              _counter++;
+              text = update["message"]["content"]["text"]["text"];
+            });
+          return await tg.request(
+              "sendMessage", {"chat_id": chatId, "text": "apa lo"});
+          }
+        }
+      }
+    });
+  }
+
+  void telegram(SendPort sendPort) async {
+    tg.on("update", (update) async {
+      sendPort.send(update);
+      if (update["@type"] == "updateNewMessage" &&
+          update["message"]["@type"] == "message") {
+        var msg = update["message"];
+        var chatId = msg["chat_id"];
+        if (!msg["is_outgoing"]) {
+          return await tg.request(
+              "sendMessage", {"chat_id": chatId, "text": "Hello world"});
+        }
+      }
+    });
+    await tg.bot("2123043767:AAEY0KTdVYo0JTRmFF5S4QPBnvoCdpe2yPI");
   }
 
   @override
@@ -94,9 +135,13 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             const Text(
               'You have pushed the button this many times:',
+            ),
+            Text(
+              text,
+              style: Theme.of(context).textTheme.headline4,
             ),
             Text(
               '$_counter',
