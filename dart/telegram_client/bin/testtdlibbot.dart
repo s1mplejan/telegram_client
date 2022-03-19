464772c4f3c3ac01c69f7b9c8140d85da159f33d
￴ <>
2022-03-19 20:31:15 +0700

    update

--- dart/telegram_client/bin/testtdlibbot.dart
+++ dart/telegram_client/bin/testtdlibbot.dart
@@ -1,26 +1,30 @@
 import 'package:telegram_client/telegram_client.dart';
+import 'dart:io';
 
 void main() async {
-  var option = {
-    'api_id': 1917085,
-    'api_hash': 'a612212e6ac3ff1f97a99b2e0f050894',
-    'database_directory':
-        "/home/azkadev/Documents/telegram_client/dart/telegram_client/bin/bot",
-    'files_directory':
-        "/home/azkadev/Documents/telegram_client/dart/telegram_client/bin/bot"
-  };
-  var tg =
-      Tdlib("/home/azkadev/Downloads/azkauserrobot-1.0.1/libtdjson.so", option);
-  tg.on("update", (update) async {
-    print(update);
-    if (update["@type"] == "updateNewMessage" &&
-        update["message"]["@type"] == "message") {
-      var msg = update["message"];
-      var chatId = msg["chat_id"];
-      if (!msg["is_outgoing"]) {
-        tg.request("sendMessage", {"chat_id": chatId, "text": "Hello world"});
+  try {
+    var path = Directory.current.path;
+    print(path);
+    var option = {
+      'api_id': 1917085,
+      'api_hash': 'a612212e6ac3ff1f97a99b2e0f050894',
+      'database_directory': "$path/bot",
+      'files_directory': "$path/bot",
+    };
+    var tg = Tdlib("$path/libtdjson.so", option);
+    tg.on("update", (update) async {
+      print(update);
+      if (update["@type"] == "updateNewMessage" &&
+          update["message"]["@type"] == "message") {
+        var msg = update["message"];
+        var chatId = msg["chat_id"];
+        if (!msg["is_outgoing"]) {
+          tg.request("sendMessage", {"chat_id": chatId, "text": "Hello world"});
+        }
       }
-    }
-  });
-  await tg.bot("2123043767:AAEY0KTdVYo0JTRmFF5S4QPBnvoCdpe2yPI");
+    });
+    await tg.bot("2123043767:AAEY0KTdVYo0JTRmFF5S4QPBnvoCdpe2yPI");
+  } catch (e) {
+    print(e);
+  }
 }
